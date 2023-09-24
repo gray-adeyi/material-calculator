72b8b1f4e97d81a77b8425c4fab4e658843b8585
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-09-24 20:36:12 +0100

    Update app with components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CalculatorDisplay from "./components/CalculatorDisplay.vue";
+import SpecialFunc from "./components/SpecialFunc.vue";
+import CalculatorKeyboard from "./components/CalculatorKeyboard.vue";
+</script>
 
 <template>
-  <div class="calculator-container"></div>
+  <div class="calculator-container">
+    <CalculatorDisplay />
+    <SpecialFunc />
+    <CalculatorKeyboard />
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .calculator-container {
-  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  margin: 1.25rem auto 0;
   width: calc(67.5rem / 3);
   height: calc(150rem / 3);
   background: var(--calculator-bg);
