a6d4ab574e6595f8b64c53941ee307690272eacb
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-09-24 20:33:56 +0100

    Implement CalculatorDisplay component

---
+++ src/components/CalculatorDisplay.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="display-container"></div>
+</template>
+
+<style lang="scss" scoped>
+.display-container {
+  min-height: calc(50.5625rem / 3);
+  background: var(--display-bg);
+  border-bottom-left-radius: 1.25rem;
+  border-bottom-right-radius: 1.25rem;
+}
+</style>
