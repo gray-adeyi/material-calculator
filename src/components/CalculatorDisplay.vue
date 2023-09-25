8839c9a1a3e79a6e49f5231e7e76409ce041b752
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-09-25 19:10:56 +0100

    Implement CalculatorDisplay component

--- src/components/CalculatorDisplay.vue
+++ src/components/CalculatorDisplay.vue
@@ -1,14 +1,32 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import CalculatorEntry from "./CalculatorEntry.vue";
+import CalculatorMenu from "./CalculatorMenu.vue";
+import BaseEntry from "./BaseEntry.vue";
+</script>
 
 <template>
-  <div class="display-container"></div>
+  <div class="display-container">
+    <CalculatorMenu />
+    <CalculatorEntry />
+    <BaseEntry class="answer-text" value="10" />
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .display-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
   min-height: calc(50.5625rem / 3);
   background: var(--display-bg);
   border-bottom-left-radius: 1.25rem;
   border-bottom-right-radius: 1.25rem;
+  padding-right: 1rem;
+  overflow: hidden;
+}
+
+.answer-text {
+  font-size: 80px;
+  max-height: 5.625rem;
 }
 </style>
